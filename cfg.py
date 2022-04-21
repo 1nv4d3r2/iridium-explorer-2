776bb23185ae09df654c8f39e6f73a057e019c6a
cappelozzi <cappelozzi@localhost.localdomain>
2022-04-21 15:51:34 -0300

    explorer commit

---
+++ cfg.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Multichain Explorer 2 (c) Coin Sciences Ltd
+# Iridium Explorer 2 (c) Iridium Developer 2023
+# All rights reserved under BSD 3-clause license
+
+settings=None
+selected=None
+feed_dir=""
+ini_dir=""
+log_file=""
+pid_file=""
+explorer_name=""
+action=None
+ini_file=""
+chains=None
+page_handler=None
+data_handler=None
+
+
